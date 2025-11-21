ed03e399f6f958f4fe3752c5fb4544b03a028db5
higorgandra <higorgandra@gmail.com>
2025-11-21 19:39:26 -0300

    cloudflare

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // [https://vitejs.dev/config/](https://vitejs.dev/config/)
 export default defineConfig({
   plugins: [react()],
-  base: "/trevvo-host/", // O nome do seu repositório aqui entre barras
+  base: "/", // O nome do seu repositório aqui entre barras
 })
